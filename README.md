9b32fd342f8d5eb0ae33ecedc199bba7cb82145a
Lennart <yxili00@163.com>
2023-04-03 21:45:17 +0800

    update20230403

--- README.md
+++ README.md
@@ -19,4 +19,8 @@ This is Lennart's course project of *Computational Mechanics*. I would call it *
 
 ## 5.Daily Record
 ### 2023 March. 29
-    I was stucked in QPainter
+    I was stucked in how to use QPainter. I write several rows of code, but there isn't any lines or rectangulars on the widget.
+
+### 2023 March.31  
+    I solve the problem above.Now I start to study how to connect the mouse event with the QPainter.
+
--- build-trueCM_QT-Desktop_Qt_6_4_2_MSVC2019_64bit-Debug/trueCM_QT_autogen/include/ui_widget.h
+++ build-trueCM_QT-Desktop_Qt_6_4_2_MSVC2019_64bit-Debug/trueCM_QT_autogen/include/ui_widget.h
@@ -11,7 +11,9 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
-#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QGroupBox>
+#include <QtWidgets/QTabWidget>
+#include <QtWidgets/QTextEdit>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -19,20 +21,48 @@ QT_BEGIN_NAMESPACE
 class Ui_Widget
 {
 public:
-    QWidget *layoutWidget;
-    QGridLayout *gridLayout_2;
+    QTabWidget *tabWidget;
+    QWidget *tab;
+    QWidget *tab_2;
+    QWidget *tab_3;
+    QWidget *tab_4;
+    QWidget *tab_5;
+    QTextEdit *textEdit;
+    QGroupBox *groupBox;
+    QWidget *widget;
 
     void setupUi(QWidget *Widget)
     {
         if (Widget->objectName().isEmpty())
             Widget->setObjectName("Widget");
         Widget->resize(645, 431);
-        layoutWidget = new QWidget(Widget);
-        layoutWidget->setObjectName("layoutWidget");
-        layoutWidget->setGeometry(QRect(0, 0, 641, 431));
-        gridLayout_2 = new QGridLayout(layoutWidget);
-        gridLayout_2->setObjectName("gridLayout_2");
-        gridLayout_2->setContentsMargins(0, 0, 0, 0);
+        tabWidget = new QTabWidget(Widget);
+        tabWidget->setObjectName("tabWidget");
+        tabWidget->setGeometry(QRect(0, 0, 191, 381));
+        tab = new QWidget();
+        tab->setObjectName("tab");
+        tabWidget->addTab(tab, QString());
+        tab_2 = new QWidget();
+        tab_2->setObjectName("tab_2");
+        tabWidget->addTab(tab_2, QString());
+        tab_3 = new QWidget();
+        tab_3->setObjectName("tab_3");
+        tabWidget->addTab(tab_3, QString());
+        tab_4 = new QWidget();
+        tab_4->setObjectName("tab_4");
+        tabWidget->addTab(tab_4, QString());
+        tab_5 = new QWidget();
+        tab_5->setObjectName("tab_5");
+        tabWidget->addTab(tab_5, QString());
+        textEdit = new QTextEdit(Widget);
+        textEdit->setObjectName("textEdit");
+        textEdit->setGeometry(QRect(0, 380, 651, 51));
+        groupBox = new QGroupBox(Widget);
+        groupBox->setObjectName("groupBox");
+        groupBox->setGeometry(QRect(190, 0, 451, 381));
+        widget = new QWidget(groupBox);
+        widget->setObjectName("widget");
+        widget->setGeometry(QRect(0, 10, 451, 371));
 
         retranslateUi(Widget);
 
@@ -42,6 +72,12 @@ public:
     void retranslateUi(QWidget *Widget)
     {
         Widget->setWindowTitle(QCoreApplication::translate("Widget", "\350\256\241\347\256\227\345\212\233\345\255\246\346\261\202\350\247\243\345\231\250", nullptr));
+        tabWidget->setTabText(tabWidget->indexOf(tab), QCoreApplication::translate("Widget", "Tab 1", nullptr));
+        tabWidget->setTabText(tabWidget->indexOf(tab_2), QCoreApplication::translate("Widget", "Tab 2", nullptr));
+        tabWidget->setTabText(tabWidget->indexOf(tab_3), QCoreApplication::translate("Widget", "\351\241\265", nullptr));
+        tabWidget->setTabText(tabWidget->indexOf(tab_4), QCoreApplication::translate("Widget", "\351\241\265", nullptr));
+        tabWidget->setTabText(tabWidget->indexOf(tab_5), QCoreApplication::translate("Widget", "\351\241\265", nullptr));
+        groupBox->setTitle(QCoreApplication::translate("Widget", "GroupBox", nullptr));
     } // retranslateUi
 
 };
